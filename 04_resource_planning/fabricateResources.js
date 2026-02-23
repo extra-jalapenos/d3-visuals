500bd0f8650115c3a4e9775e7f332bce4aa2a5f4
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2026-02-23 10:36:53 +0100

    add today argument for manipulating resources

--- 04_resource_planning/fabricateResources.js
+++ 04_resource_planning/fabricateResources.js
@@ -1,9 +1,10 @@
 import { getWorkerHoursForTimeframe } from "./estimated-resources.js"
 // takes a workscheme with Monday, Tuesday, etc... and generates slightly varying hours
-export const fabricateWorkerHours = (fromDate, toDate, workscheme) => {
+export const fabricateWorkerHours = (fromDate, toDate, today = new Date()) => {
 	const actualHours = getWorkerHoursForTimeframe(fromDate, toDate)
 	// const divergingHours = d3.randomWeibull(1.1) // extreme fluctuation
 	const divergingHours = d3.randomWeibull(10) // somewhat realistic fluctuations
-	actualHours.forEach(entry => entry["hours"] *= divergingHours())
+	// in case of future dates, NO fluctuation assumed
+	actualHours.forEach(entry => entry["hours"] *= entry.date <= today ? divergingHours() : 1)
 	return actualHours
 }
