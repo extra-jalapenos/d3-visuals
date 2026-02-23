29393b8e4d735222fc6fd645b97b9bb712af0112
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2026-02-23 10:49:29 +0100

    improve visibility of points

--- 04_resource_planning/accumulatedPoints.js
+++ 04_resource_planning/accumulatedPoints.js
@@ -6,11 +6,13 @@ export const getPointsByDate = async (startDate, endDate, today = new Date()) =>
     const worklogDataRaw = await d3.tsv("/00_constants/data/worklogs.txt")
     const worklogData = cleanWorklogData(worklogDataRaw)
     const worklogDataFiltered = worklogData.filter(
-        data => !!data.doneAt && data.doneAt <= today && data.doneAt >= startDate && data.doneAt <= endDate
+        data => !!data.doneAt
+            && data.doneAt <= today
+            && data.doneAt >= startDate
+            && data.doneAt <= endDate
     )
     const steps = await loadSteps()
-
-    // group by date
+    // sum by date
     const byDateAndStep = d3.rollup(worklogDataFiltered, D => d3.sum(D.map(d => steps.get(d.workstep).Punkte)), d => d.doneAt)
     return byDateAndStep
 }
